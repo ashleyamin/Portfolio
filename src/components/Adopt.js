39a991f1c92d36e60ebe7768a5a0b2af076997b4
Ashley Amin <ashley.starks@gmail.com>
2018-04-16 14:17:04 -0400

    finished adopt project

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import adopt from '../assets/adopt.png';
+import code from '../assets/adopt/code.png';
+import manage from '../assets/adopt/manage.png';
+import routes from '../assets/adopt/routes.jpeg';
+
 
 class Adopt extends Component {
   render() {
     return (
       <div className="Adopt">
-        <img className="Image-big-adopt" src={adopt} />
-        <h1 className="Title">adopt a pet</h1>
+        <img className="Image-big-adopt-cover" src={adopt} />
+        <h1 className="Title">adopt a pet prototype</h1>
         <p className="project-description">Code: Reactjs, Webpack, Nodejs</p>
         <div className="project-description-long">
-              Making it easier to find and adopt more puppies!
-              A group project in our web development immersive program, the PuppyFinder App was built
-              collaboratively using React on the front end, Webpack as the build manager, Node.js and the Express
-              framework on the backend, with postgres as the database.
+              A CRUD App, using React and utilizing the Pet Finder API. A user can search for
+              dogs to adopt by breed and zipcode. When the user finds a dog they may like,
+              they can save it to a favorites list and edit their interest in the dog, add notes
+              and/or remove from the puppy from the favorites list.
+              <br />
+              <br />
+              A group project in our web development immersive program at general assembly, we
+              used Trello for project management, developed whiteboard sketches for each screen,
+              and defined our React routes and components prior to jumping into development.
+              The PuppyFinder App was built collaboratively using React on the front end,
+              webpack as the build manager, Node.js and the Express framework on the backend,
+              with postgres as the database.
         </div>
         <a className="Learn-more-button" href="https://puppyfinderapp.herokuapp.com/" target="_blank">see the project</a>
         <a className="Github-button" href="https://github.com/ashleyamin/Puppy-Finder" target="_blank">see the code</a>
+        <img className="Image-big-adopt" src={manage} />
+        <img className="Image-big-adopt" src={routes} />
+        <img className="Image-big-adopt" src={code} />
       </div>
     );
   }
--- src/components/Stoop.js
+++ src/components/Stoop.js
@@ -7,13 +7,22 @@ class Stoop extends Component {
     return (
       <div className="Stoop">
         <img className="Image-big-stoop" src={stoop} />
-        <h1 className="Title">the stoop app</h1>
-        <p className="project-description">Code: Ruby and Rails</p>
+        <h1 className="Title">the stoop app prototype</h1>
+        <p className="tagline-essentials">Code: Ruby and Rails</p>
         <div className="project-description-long">
-        The Stoop App is a collaborative tool that assists individuals in identifying
-        and sharing experiences about civictech, government and community provided benefits, products
-        and services in NYC. I built this using Ruby on Rails and a custom set of data utilizing three
-        related table structures.
+        Inspired by the growing number of civic and political activism digital products and services
+        available for citizens and orgnizers, the stoop app prototype intends to be a collaborative tool
+        (e.g. Yelp, Product Hunt) that assists individuals in identifying and sharing experiences
+        and thoughts on these products in NYC.
+        <br />
+        <br />
+        I built this using Ruby on Rails and a custom set of
+        data utilizing three related table structures. The main features include the ability
+        to create a new product, create a new experience review, and edit/update the reviews.
+        <br />
+        <br />
+        Future updates will include creating user accounts, product/service owner accounts, and administration
+        capabilities for monitoring posts.
         </div>
         <a className="Learn-more-button" href="http://thestoop.herokuapp.com/" target="_blank">see the project</a>
         <a className="Github-button" href="https://github.com/ashleyamin/The-Stoop" target="_blank">see the code</a>
